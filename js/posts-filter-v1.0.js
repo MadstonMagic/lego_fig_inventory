08aa4635f51b47bd81b897f5e889f5efb33d311b
MadstonMagic <sapphirecraft.help@gmail.com>
2020-11-20 12:05:50 +0100

    Mobile Ready

--- js/posts-filter-v1.0.js
+++ js/posts-filter-v1.0.js
@@ -1,18 +1,33 @@
 $(document).ready(function(){
 	//Show all categories
 	$('.cat_all').on('click',function() {
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_all').css("color", "var(--white)");
+		
+		
 		$('.over-mij-wrapper').css("display","none");
 		$('.single-post.all').css("display","block");
 	});
 	
 	//Show Over Mij block
 	$('.cat_over_mij').on('click',function(){
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_over_mij').css("color", "var(--white)");
+		
+		
 		$('.over-mij-wrapper').css("display","block");
 		$('.single-post.all').css("display","none");
 	});
 	
 	//Show Gun Category
 	$('.cat_gun').on('click',function(){
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_gun').css("color", "var(--white)");
+		
+		
 		$('.over-mij-wrapper').css("display","none");
 		$('.single-post').css("display","block");
 		$('.single-post.all:not(.gun)').css("display","none");
@@ -20,6 +35,11 @@ $(document).ready(function(){
 	
 	//Show Weapon Category
 	$('.cat_weapon').on('click',function(){
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_weapon').css("color", "var(--white)");
+		
+		
 		$('.over-mij-wrapper').css("display","none");
 		$('.single-post').css("display","block");
 		$('.single-post.all:not(.weapon)').css("display","none");
@@ -27,6 +47,11 @@ $(document).ready(function(){
 	
 	//Show Animal/Monster Category
 	$('.cat_animal_monster').on('click',function(){
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_animal_monster').css("color", "var(--white)");
+		
+		
 		$('.over-mij-wrapper').css("display","none");
 		$('.single-post').css("display","block");
 		$('.single-post.all:not(.animal_monster)').css("display","none");
@@ -34,6 +59,11 @@ $(document).ready(function(){
 	
 	//Show Misc Category
 	$('.cat_misc').on('click',function(){
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_misc').css("color", "var(--white)");
+		
+		
 		$('.over-mij-wrapper').css("display","none");
 		$('.single-post').css("display","block");
 		$('.single-post.all:not(.misc)').css("display","none");
@@ -41,6 +71,11 @@ $(document).ready(function(){
 	
 	//Show WaveFront Category
 	$('.cat_wavefront').on('click',function(){
+		
+		$('.menuList_item').css("color", "var(--blue)");
+		$('.cat_wavefront').css("color", "var(--white)");
+		
+		
 		$('.over-mij-wrapper').css("display","none");
 		$('.single-post').css("display","block");
 		$('.single-post.all:not(.wavefront)').css("display","none");
